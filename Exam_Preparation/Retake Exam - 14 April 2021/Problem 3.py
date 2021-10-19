76481dea0627417063ada2e3d642fab79d16842e
PetarSP <petar.stefkov@gmail.com>
2021-10-19 08:03:38 +0300

    functions advanced

--- Exam_Preparation/Retake Exam - 14 April 2021/Problem 3.py
+++ Exam_Preparation/Retake Exam - 14 April 2021/Problem 3.py
@@ -13,6 +13,6 @@ def flights(*args):
                     result[args[key]] += int(args[value])
                     break
 
-# print(flights('Vienna', 256, 'Vienna', 26, 'Morocco', 98, 'Paris', 115, 'Finish', 'Paris', 15))
-# print(flights('London', 0, 'New York', 9, 'Aberdeen', 215, 'Sydney', 2, 'New York', 300, 'Nice', 0, 'Finish'))
+print(flights('Vienna', 256, 'Vienna', 26, 'Morocco', 98, 'Paris', 115, 'Finish', 'Paris', 15))
+print(flights('London', 0, 'New York', 9, 'Aberdeen', 215, 'Sydney', 2, 'New York', 300, 'Nice', 0, 'Finish'))
 print(flights('Finish', 'New York', 90, 'Aberdeen', 300, 'Sydney', 0))
